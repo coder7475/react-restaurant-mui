81aa5ac7af4fa755374ecb286957ce628951172a
coder7475 <robiulhossain7475@gmail.com>
2023-11-20 10:53:44 +0600

    feat: add the banner and title to our shop page

--- src/pages/OurShop.jsx
+++ src/pages/OurShop.jsx
@@ -1,7 +1,20 @@
+
+import { Helmet } from 'react-helmet-async';
+import Description from '../components/Description';
+import img from "./../assets/shop/banner2.jpg";
+
+
 const OurShop = () => {
   return (
     <div>
-      SHOP
+      <Helmet>
+        <title>Bistro Boss | SHOP</title>
+      </Helmet>
+      <Description
+        img={img}
+        subHeading="Would you like to try a dish?"
+        heading="OUR SHOP"
+      />
     </div>
   );
 };
